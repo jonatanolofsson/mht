2d4234974203a031be3d3eaebaeb6f59dc169bac
Jonatan Olofsson <jonatan.olofsson@gmail.com>
2016-10-13 17:21:01 +0200

    Add README and license

---
+++ README.md
@@ -0,0 +1,5 @@
+# Multiple Hypothesis Tracking
+
+
+# License
+This software is released under the GPLv3 license.
--- mht/cluster.py
+++ mht/cluster.py
@@ -1,4 +1,20 @@
 """MHT Cluster."""
+
+"""
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
 import queue
 from math import log, exp
 import numpy as np
