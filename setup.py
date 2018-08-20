c4af9c313c4e44ca23edd418b5281618fc29693d
Jonatan Olofsson <jonatan.olofsson@gmail.com>
2018-08-20 17:59:47 +0200

    Py37

---
+++ setup.py
@@ -0,0 +1,51 @@
+"""A setuptools based setup module.
+
+See:
+https://packaging.python.org/en/latest/distributing.html
+https://github.com/pypa/sampleproject
+"""
+
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+# To use a consistent encoding
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+setup(
+    name='mht',
+    version='1.0.0',
+
+    description='A Multiple Hypothesis Tracker',
+
+    # The project's main homepage.
+    url='https://github.com/jonatanolofsson/mht',
+
+    # Author details
+    author='Jonatan Olofsson',
+    author_email='jonatan.olofsson@gmail.com',
+
+    # Choose your license
+    license='GPLv3',
+
+    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 4 - Beta',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 3.7',
+    ],
+
+    # What does your project relate to?
+    keywords='tracking multi-target',
+
+    # You can just specify the packages manually here if your project is
+    # simple. Or you can use find_packages().
+    packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
+)
